529350a52dd6324f074789b4051b1340e52dc05a
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-10-24 12:06:54 +0200

    fix: e2e corrected RPC calls

--- e2e/src/App.tsx
+++ e2e/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
   const [walletName, setWalletName] = useState("")
   const [wallet, setWallet] = useState<StarknetWindowObject | null>(null)
   const [walletAccount, setWalletAccount] = useState<WalletAccount | null>(null)
+  const [walletAddress, setWalletAddress] = useState<string | null>(null)
   const [result, setResult] = useState<string | boolean>("") // State to hold the result for display
 
   async function handleConnect(options?: ConnectOptions) {
@@ -36,7 +37,19 @@ function App() {
     await disconnect(options)
     setWalletName("")
     setWallet(null)
+    setWalletAddress(null)
     setResult("") // Clear result on disconnect
+  }
+
+  const tokenToWatch = {
+    type: "ERC20",
+    options: {
+      address:
+        "0x044e6bcc627e6201ce09f781d1aae44ea4c21c2fdef299e34fce55bef2d02210",
+      name: "Lords",
+      symbol: "LORDS",
+      decimals: 18,
+    },
   }
 
   const methods = [
@@ -46,16 +59,7 @@ function App() {
       accountWallet: async () => {
         if (walletAccount) {
           try {
-            const response = await walletAccount.watchAsset({
-              type: "ERC20",
-              options: {
-                address:
-                  "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-                name: "Ether",
-                symbol: "ETH",
-                decimals: 18,
-              },
-            })
+            const response = await walletAccount.watchAsset(tokenToWatch as any)
             setResult(JSON.stringify(response, null, 2)) // Format as JSON
           } catch (error: any) {
             console.log(error)
@@ -68,17 +72,7 @@ function App() {
           try {
             const response = await wallet.request({
               type: "wallet_watchAsset",
-              params: {
-                type: "ERC20",
-                options: {
-                  address:
-                    "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-                  name: "Ether",
-                  symbol: "ETH",
-                  decimals: 18,
-                },
-                api_version: "0.7",
-              },
+              params: tokenToWatch as any,
             })
             setResult(JSON.stringify(response, null, 2)) // Format as JSON
           } catch (error: any) {
@@ -158,6 +152,7 @@ function App() {
               type: "wallet_requestAccounts",
             })
             setResult(JSON.stringify(response, null, 2)) // Format as JSON
+            setWalletAddress(response[0])
           } catch (error: any) {
             console.log(error)
             setResult(`Error: ${error.message}`)
@@ -194,12 +189,20 @@ function App() {
       label: "Add Invoke Transaction",
       accountWallet: null, // Not implemented
       rpc: async () => {
-        if (wallet) {
+        console.log(walletAddress)
+        if (wallet && walletAddress) {
           try {
             const response = await wallet.request({
               type: "wallet_addInvokeTransaction",
               params: {
-                /* invoke transaction params */
+                calls: [
+                  {
+                    contract_address:
+                      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                    entry_point: "transfer",
+                    calldata: [walletAddress, "1000000000000000", "0"],
+                  },
+                ],
               },
             })
             setResult(JSON.stringify(response, null, 2)) // Format as JSON
@@ -240,7 +243,41 @@ function App() {
           try {
             const response = await wallet.request({
               type: "wallet_signTypedData",
-              params: { domain: {}, types: {}, message: {} },
+              params: {
+                types: {
+                  StarkNetDomain: [
+                    { name: "name", type: "felt" },
+                    { name: "version", type: "felt" },
+                    { name: "chainId", type: "felt" },
+                  ],
+                  Person: [
+                    { name: "name", type: "felt" },
+                    { name: "wallet", type: "felt" },
+                  ],
+                  Mail: [
+                    { name: "from", type: "Person" },
+                    { name: "to", type: "Person" },
+                    { name: "contents", type: "felt" },
+                  ],
+                },
+                primaryType: "Mail",
+                domain: {
+                  name: "Starknet Mail",
+                  version: "1",
+                  chainId: 1,
+                },
+                message: {
+                  from: {
+                    name: "Cow",
+                    wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+                  },
+                  to: {
+                    name: "Bob",
+                    wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+                  },
+                  contents: "Hello, Bob!",
+                },
+              },
             })
             setResult(JSON.stringify(response, null, 2)) // Format as JSON
           } catch (error: any) {
